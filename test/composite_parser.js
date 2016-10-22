da591b8aaaf612baceb9b2a6468ffbaa2821f6ac
Ulric Wilfred <shaman.sir@gmail.com>
2016-10-22 09:54:32 +0200

    make parsers be able to reference other parsers by name

--- test/composite_parser.js
+++ test/composite_parser.js
@@ -381,6 +381,40 @@ describe('Composite parser', function(){
                 }
             });
         });
+        it('should be able to refer to other parsers by name', function(){
+            var parser = Parser.start().namely('self');
+
+            var stop = Parser.start().namely('stop');
+
+            var twoCells = Parser.start().namely('twoCells')
+                                         .nest('left',  { type: 'self' })
+                                         .nest('right', { type: 'stop' })
+
+            parser
+                .uint8('type')
+                .choice('data', {
+                    'tag': 'type',
+                    'choices': {
+                        0: 'stop',
+                        1: 'self',
+                        2: 'twoCells'
+                    }
+                });
+
+            var buffer = new Buffer([ 2,
+                                        /* left */  1, 1, 0,
+                                        /* right */ 0 ]);
+            assert.deepEqual(parser.parse(buffer), {
+                type: 2,
+                data: {
+                    left: {
+                        type: 1,
+                        data: { type: 1, data: { type: 0, data: {} } },
+                    },
+                    right: { }
+                }
+            });
+        });
         it('should be able to go into recursion with complex nesting', function(){
             var stop = Parser.start();
 
