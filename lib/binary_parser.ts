d8f1a31870c187762be3ada544d999524a05f403
Nick Hunter <nick@nickhunter.com>
2023-06-20 14:18:43 +0000

    Fix formatting issues. Add tests

--- lib/binary_parser.ts
+++ lib/binary_parser.ts
@@ -286,7 +286,7 @@ export class Parser {
   alias?: string;
   useContextVariables = false;
 
-  constructor() { }
+  constructor() {}
 
   static start() {
     return new Parser();
@@ -1046,13 +1046,7 @@ export class Parser {
         if (this.next.options && this.next.options.type instanceof Parser) {
           // Something in the nest
           if (this.next.options.type.next) {
-            if (this.next.options.type.next.type === "bit") {
-              // Next is a bit field
-              return false;
-            } else {
-              // Next is something elses
-              return true;
-            }
+            return this.next.options.type.next.type !== "bit";
           }
           return false;
         } else {
@@ -1077,10 +1071,7 @@ export class Parser {
     parser.varName = ctx.generateVariable(parser.varName);
     ctx.bitFields.push(parser);
 
-    if (
-      !this.next ||
-      this.nextNotBit()
-    ) {
+    if (!this.next || this.nextNotBit()) {
       const val = ctx.generateTmpVariable();
 
       ctx.pushCode(`var ${val} = 0;`);
@@ -1127,7 +1118,8 @@ export class Parser {
           if (rem) {
             const mask = -1 >>> (32 - rem);
             ctx.pushCode(
-              `${parser.varName} = (${val} & 0x${mask.toString(16)}) << ${length - rem
+              `${parser.varName} = (${val} & 0x${mask.toString(16)}) << ${
+                length - rem
               };`
             );
             length -= rem;
@@ -1139,7 +1131,8 @@ export class Parser {
         const mask = -1 >>> (32 - length);
 
         ctx.pushCode(
-          `${parser.varName} ${length < (parser.options.length as number) ? "|=" : "="
+          `${parser.varName} ${
+            length < (parser.options.length as number) ? "|=" : "="
           } ${val} >> ${offset} & 0x${mask.toString(16)};`
         );
 
